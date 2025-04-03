d75c621a45904f8d452398760f0973e376cee0ce
Nicolas <nicolasdelfino585@gmail.com>
2025-04-03 15:37:04 -0300

    front main page almost done

---
+++ turnero-peluqueria/src/App.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      {/* Contenedor centrado con ancho fijo */}
+      <div
+        className="container d-flex flex-column min-vh-100"
+        style={{ maxWidth: "720px", height: "411.6px" }}
+      >
+        {/* Imagen ajustada al ancho del contenedor */}
+        <div className="foto-header w-100">
+          <img src="pelu.jpg" alt="Peluquería" className="w-100 h-auto" />
+        </div>
+        <div className="container-titulo">
+          <h1 className="titulo">Ivan Belotti Salón Masculino </h1>
+        </div>
+        <hr />
+        <div className="container d-flex ">
+          <div className="column info text-start mt-4">
+            <p>
+              <i className="fa-solid fa-location-dot me-2" /> Vélez Sarsfield
+              410 - Local 4 | Las Varillas, Córdoba
+            </p>
+            <p>
+              <i class="fa-regular fa-calendar me-2" /> Martes a Viernes: 8:30 -
+              12:00 | Sábados: 8:30 - 13:00 & 17:30 - 20:30
+            </p>
+            <p>
+              <i class="fa-brands fa-whatsapp me-2" />
+              3533 438227
+            </p>
+          </div>
+        </div>
+        <hr />
+      </div>
+      <footer className="mt-auto bg-light py-3">
+        <div className="container d-flex align-items-center justify-content-center whatsapp">
+          <span>
+            <i class="fa-brands fa-lg fa-instagram me-2"></i> ivanbelloti78
+          </span>
+        </div>
+      </footer>
+    </>
+  );
+}
+export default App;
